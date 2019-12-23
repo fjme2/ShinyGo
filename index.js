56b401382cb5257a6ddd91bf656cdcc2d7a8f514
Fran Maciá <fjme2@alu.ua.es>
2019-12-23 20:41:10 +0100

    Prueba inicio

--- index.js
+++ index.js
@@ -2,15 +2,20 @@ require('dotenv').config({path: __dirname + '/.env'});
 var express = require("express");
 const Telegraf = require("telegraf");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const mensajeBienvenida = `Bienvenido a ShinyGo Bot!
 Para ver mis funciones usa el comando /help`;
 
-bot.start(ctx => ctx.reply(mensajeBienvenida));
-bot.launch();
-
 var app = express();
+app.get('/', (req, res) => {
+  bot.start(ctx => ctx.reply(mensajeBienvenida));
+  bot.launch();
+});
+
+/*bot.start(ctx => ctx.reply(mensajeBienvenida));
+bot.launch();*/
+
 app.listen(PORT, () => {
   console.log(`REST API on http://localhost:${PORT}`);
 });
