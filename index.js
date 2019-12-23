821b678bef078151906814a0b8de28a7ec09d807
Fran Maciá <fjme2@alu.ua.es>
2019-12-23 18:24:44 +0100

    Empezamos de nuevo

--- index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var bp = require('body-parser');
-
-var app = express();
-app.use(bp.json());
-
-app.listen(3000, function () {
-    console.log("El servidor express está en el puerto 3000");
-});
